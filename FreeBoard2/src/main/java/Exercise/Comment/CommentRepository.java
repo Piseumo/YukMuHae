b69229c4be0dd34d7ad0a130308da24e1d668947
DESKTOP-KVLCEEB\user <tackteho@naver.com>
2024-09-23 00:54:35 +0900

    no message

--- FreeBoard2/src/main/java/Exercise/Comment/CommentRepository.java
+++
@@ -1,6 +0,0 @@
-package Exercise.Comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment , Long> {
-}
--- FreeBoard2/src/main/java/Exercise/FreeBoard/FreeBoardRepository.java
+++
@@ -1,6 +0,0 @@
-package Exercise.FreeBoard;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FreeBoardRepository extends JpaRepository<FreeBoard,Long> {
-}
