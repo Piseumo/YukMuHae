728697818b60e9d6c127666fd70616a5df36669f
DESKTOP-KVLCEEB\user <tackteho@naver.com>
2024-09-23 00:55:08 +0900

    Update comment logic

---
+++ FreeBoard2/src/main/java/Exercise/FreeBoard/FreeBoardRepository.java
@@ -0,0 +1,6 @@
+package Exercise.FreeBoard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FreeBoardRepository extends JpaRepository<FreeBoard,Long> {
+}
---
+++ FreeBoard2/src/test/java/Exercise/FreeBoard/FreeBoard2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Exercise.FreeBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeBoard2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
